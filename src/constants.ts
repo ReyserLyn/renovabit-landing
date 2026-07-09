7e8523fdf674f3d1911476abfd46834eb3112199
Reyser Zapata <reyserlyn@gmail.com>
2026-07-09 17:48:36 -0500

    chore(repo): initial scaffolding with astro 7, biome, preact, hugeicons

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+	name: "RenovaBit",
+	url: "https://renovabit.com",
+	description:
+		"Servicio técnico de laptops y PCs en Arequipa. Reparación, mantenimiento y soporte remoto.",
+	author: "RenovaBit",
+	locale: "es_PE",
+	ogImage: "/og-default.png",
+} as const;
